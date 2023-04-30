622eb05fba539b7887c47f362de3d1ef04f06b16
Андреј Зафировски <Andrej.Zafirovski@students.finki.ukim.mk>
2023-04-30 10:58:19 +0200

    Initial folder structure

---
+++ src/app/modules/snakers/snakers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SnakersRoutingModule } from './snakers-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SnakersRoutingModule
+  ]
+})
+export class SnakersModule { }
