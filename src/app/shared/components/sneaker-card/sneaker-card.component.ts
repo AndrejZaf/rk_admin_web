a2c74a7ef2e04f7baa1a5224de95f5f246c52feb
Андреј Зафировски <Andrej.Zafirovski@students.finki.ukim.mk>
2023-12-02 22:02:25 +0100

    Modify the sneaker card to keep the spacing if some information is hidden

--- src/app/shared/components/sneaker-card/sneaker-card.component.ts
+++ src/app/shared/components/sneaker-card/sneaker-card.component.ts
@@ -6,8 +6,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./sneaker-card.component.scss'],
 })
 export class SneakerCardComponent {
+  showInfo: boolean = false;
   @Input() sneakerName: string = '';
   @Input() sneakerUrl: string = '';
   @Input() sneakerPrice: number = 0;
   @Input() sneakerDescription: string = '';
+
+  showAdditionalInfo() {
+    this.showInfo = true;
+  }
+
+  hideAdditionalInfo() {
+    this.showInfo = false;
+  }
 }
