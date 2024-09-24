0ce6de0dad2156cc256685a2fdd0ddd344283804
AZafirovski <andrej.zafirovski@students.finki.ukim.mk>
2024-09-24 13:37:28 +0200

    Make the orderService to use the environment file

--- src/app/modules/orders/services/order.service.ts
+++ src/app/modules/orders/services/order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { OrderDTO } from '../dto/order.dto';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class OrderService {
   constructor(private http: HttpClient) {}
 
   loadOrders(): Observable<OrderDTO[]> {
-    return this.http.get<OrderDTO[]>('http://localhost:8080/api/orders');
+    return this.http.get<OrderDTO[]>(`${environment.apiUrl}/api/orders`);
   }
 }
