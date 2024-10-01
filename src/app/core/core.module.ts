ff987c3b800a74cd0b9ec5978a485f0f04bd903d
AZafirovski <andrej.zafirovski@students.finki.ukim.mk>
2024-10-01 21:36:49 +0200

    Implement loader component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,21 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { throwIfAlreadyLoaded } from './guard/module-import.guard';
+import { LoaderComponent } from './components/loader/loader.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequestInterceptor } from './interceptors/http-request.interceptor';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoaderComponent],
+  exports: [LoaderComponent],
   imports: [CommonModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
